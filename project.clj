71f767733be30db4e5368d335e19c044c6fb9c29
ejschoen <ejschoen@i2kconnect.com>
2026-06-05 15:38:26 -0500

    Freeze 1.4 branch

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject jdbc-ring-session "1.4.1"
+(defproject jdbc-ring-session "1.4.2"
   :description "Ring JDBC Session Store"
   :url "https://github.com/yogthos/jdbc-ring-session"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
  :dependencies [[org.clojure/clojure "1.10.1"]
                 [ring/ring-core "1.8.2"]
-                [com.taoensso/nippy "3.0.0"]
+                [com.taoensso/encore "2.122.0"]
+                [com.taoensso/nippy "2.15.0"]
                 [commons-codec/commons-codec "1.15"]
                 [org.clojure/java.jdbc "0.7.11"]]
 
